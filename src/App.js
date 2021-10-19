888e0fb48e024db7fd6f50be36ecdaa5f8f1c84d
Lunkaz <marialbornozapata@gmail.com>
2021-10-18 21:33:08 -0300

    adding onChange & onSubmit functions to app

--- src/App.js
+++ src/App.js
@@ -11,14 +11,34 @@ class App extends Component {
     };
   }
 
+  handleChange = (e) => {
+    this.setState({
+      task : {
+        text : e.target.value,
+      }
+    })
+  }
+
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setState({
+      tasks : this.state.tasks.concat(this.state.task),
+      task: { text : ""},
+    })
+  }
+
   render() {
     const { task, tasks } = this.state;
 
     return (
       <div>
-        <form>
+        <form onSubmit={this.onSubmitTask}>
           <label htmlFor="taskInput">Enter task</label>
-          <input type="text" id="taskInput"/>
+          <input 
+            type="text"
+            id="taskInput"
+            onChange={this.handleChange}
+            value={task.text}/>
           <button type="submit">
             Add Task
           </button>
